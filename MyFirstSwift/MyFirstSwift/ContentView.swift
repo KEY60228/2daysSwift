3eada1610275f8eb8696f6a569b87607705e432f
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-07-15 18:50:53 +0900

    6-3-2. Buttonに[Modifier]を追加

--- MyFirstSwift/MyFirstSwift/ContentView.swift
+++ MyFirstSwift/MyFirstSwift/ContentView.swift
@@ -15,6 +15,9 @@ struct ContentView: View {
                 .padding()
             Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
                 Text("切り替えボタン")
+                    .foregroundColor(Color.white)
+                    .padding(.all)
+                    .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.blue/*@END_MENU_TOKEN@*/)
             }
         }
     }
