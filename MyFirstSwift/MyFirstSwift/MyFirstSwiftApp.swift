59178c147ea780afb3307a7b57ae2ab96d46c3a4
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-07-15 18:43:15 +0900

    first commit

---
+++ MyFirstSwift/MyFirstSwift/MyFirstSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstSwiftApp.swift
+//  MyFirstSwift
+//
+//  Created by 山口健太 on 2021/07/15.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
