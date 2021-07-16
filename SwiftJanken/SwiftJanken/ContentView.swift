a0e1198b6389227c62d6789d376cd5a8242cbab6
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-07-16 22:20:50 +0900

    4-1-6. Viewのデザインを整える

--- SwiftJanken/SwiftJanken/ContentView.swift
+++ SwiftJanken/SwiftJanken/ContentView.swift
@@ -12,9 +12,11 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
+            Spacer()
             // じゃんけんしていない場合
             if answerNumber == 0 {
                 Text("これからじゃんけんをします。")
+                    .padding(.bottom)
             // グー
             } else if answerNumber == 1 {
                 // グーの画像を配置
@@ -23,19 +25,25 @@ struct ContentView: View {
                     .resizable()
                     // アスペクト
                     .aspectRatio(contentMode: .fit)
+                Spacer()
                 Text("グー")
+                    .padding(.bottom)
             // チョキ
             } else if answerNumber == 2 {
                 Image("choki")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
+                Spacer()
                 Text("チョキ")
+                    .padding(.bottom)
             // パー
             } else {
                 Image("pa")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
+                Spacer()
                 Text("パー")
+                    .padding(.bottom)
             }
             Button(action: {
                 var newAnswerNumber = 0
@@ -45,6 +53,11 @@ struct ContentView: View {
                 answerNumber = newAnswerNumber
             }) {
                 Text("じゃんけんをする！")
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 100)
+                    .font(.title)
+                    .background(Color.pink)
+                    .foregroundColor(Color.white)
             }
         }
     }
