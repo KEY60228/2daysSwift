8855f0d47daa27330f58aead8f01bde35700bca2
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-07-17 13:51:31 +0900

    create project

---
+++ SwiftMusic/SwiftMusic/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftMusic
+//
+//  Created by 山口健太 on 2021/07/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftMusic/SwiftMusic/SwiftMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMusicApp.swift
+//  SwiftMusic
+//
+//  Created by 山口健太 on 2021/07/17.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
