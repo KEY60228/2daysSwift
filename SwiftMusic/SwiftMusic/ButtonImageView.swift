bfb4675e9227589e0c18f48a4f398bba3daf9862
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-07-18 18:31:52 +0900

    4-1-1. 共通化するViewを作成する

---
+++ SwiftMusic/SwiftMusic/ButtonImageView.swift
@@ -0,0 +1,20 @@
+//
+//  ButtonImageView.swift
+//  SwiftMusic
+//
+//  Created by 山口健太 on 2021/07/18.
+//
+
+import SwiftUI
+
+struct ButtonImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ButtonImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        ButtonImageView()
+    }
+}
