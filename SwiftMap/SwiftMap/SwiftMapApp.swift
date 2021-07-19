d260820a927bfead0ecc3e9edacc10e0ed1b8d6e
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-07-19 20:24:48 +0900

    create project

---
+++ SwiftMap/SwiftMap/SwiftMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMapApp.swift
+//  SwiftMap
+//
+//  Created by 山口健太 on 2021/07/19.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
