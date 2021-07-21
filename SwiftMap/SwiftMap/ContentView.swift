8fa23c7599cf369715f255a65db53c892750f433
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-07-21 22:14:07 +0900

    4-1-3. マップ種類切り替えボタンの追加

--- SwiftMap/SwiftMap/ContentView.swift
+++ SwiftMap/SwiftMap/ContentView.swift
@@ -23,6 +23,27 @@ struct ContentView: View {
 
             ZStack (alignment: .bottomTrailing) {
                 MapView(searchKey: dispSearchKey, mapType: dispMapType)
+                Button(action: {
+                    if dispMapType == .standard {
+                        dispMapType = .satellite
+                    } else if dispMapType == .satellite {
+                        dispMapType = .hybrid
+                    } else if dispMapType == .hybrid {
+                        dispMapType = .satelliteFlyover
+                    } else if dispMapType = .satelliteFlyover {
+                        dispMapType = .hybridFlyover
+                    } else if dispMapType = .hybridFlyover {
+                        dispMapType = .mutedStandard
+                    } else {
+                        dispMapType = .standard
+                    }
+                }) {
+                    Image(systemName: "map")
+                        .resizable()
+                        .frame(width: 35.0, height: 35.0, alignment: .leading)
+                }
+                .padding(.trailing, 20.0)
+                .padding(.bottom, 30.0)
             }
         }
     }
