cf4aab5ac1f5cbe0cd18b5d11a7e07fbe7c73a6d
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-07-22 20:09:50 +0900

    2-4-3. 設定画面のPickerを配置する

--- SwiftTimer/SwiftTimer/SettingView.swift
+++ SwiftTimer/SwiftTimer/SettingView.swift
@@ -8,10 +8,29 @@
 import SwiftUI
 
 struct SettingView: View {
+    @State var timerValue = 10
+
     var body: some View {
         ZStack {
             Color("backgroundSetting")
                 .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+
+            VStack {
+                Picker(selection: $timerValue, label: Text("選択")) {
+                    Text("10")
+                        .tag(10)
+                    Text("20")
+                        .tag(20)
+                    Text("30")
+                        .tag(30)
+                    Text("40")
+                        .tag(40)
+                    Text("50")
+                        .tag(50)
+                    Text("60")
+                        .tag(60)
+                }
+            }
         }
     }
 }
