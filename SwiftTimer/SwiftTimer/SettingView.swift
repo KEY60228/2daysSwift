9ac7ca1d7fe747698443172f2a5511f989457f34
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-07-22 19:37:04 +0900

    2-2-1. 秒数設定画面を追加しよう

---
+++ SwiftTimer/SwiftTimer/SettingView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingView.swift
+//  SwiftTimer
+//
+//  Created by 山口健太 on 2021/07/22.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingView()
+    }
+}
