495ce799753622040971b667c97ae91528e0de68
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-07-24 23:08:17 +0900

    2-3-6. CoordinatorクラスとUIViewControllerRepresentableプロトコルに必要なメソッドの追加

--- SwiftCamera/SwiftCamera/ImagePickerView.swift
+++ SwiftCamera/SwiftCamera/ImagePickerView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ImagePickerView: UIViewCOntrollerRepresentable {
+struct ImagePickerView: UIViewControllerRepresentable {
     // UIImagePickerController (写真撮影画) が表示されているか
     @Binding var isShowSheet: Bool
     // 撮影した写真
@@ -31,5 +31,28 @@ struct ImagePickerView: UIViewCOntrollerRepresentable {
         func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
             parent.isShowSheet = false
         }
+    }
+    
+    // Coordinatorの生成。SwiftUIによって自動的に呼び出される
+    func makeCoordinator() -> Coordinator {
+        // Coordinatorクラスのインスタンスを生成
+        Coordinator(self)
+    }
+    
+    // Viewを生成する時に実行される
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePickerView>) -> UIImagePickerController {
+        // UIImagePickerControllerのインスタンスを生成
+        let myImagePickerController = UIImagePickerController()
+        // sourceTypeにcameraを設定
+        myImagePickerController.sourceType = .camera
+        // delegate設定
+        myImagePickerController.delegate = context.coordinator
+        // UIImagePickerControllerを返す
+        return myImagePickerController
+    }
+    
+    // Viewが更新された時に実行
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePickerView>) {
+        
     }
 }
