fca0e00afbda1e4c6e3318d518acd0837529e33f
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-07-24 22:53:08 +0900

    2-3-4. 他の画面と状態を共有するためのBindingを宣言

--- SwiftCamera/SwiftCamera/ImagePickerView.swift
+++ SwiftCamera/SwiftCamera/ImagePickerView.swift
@@ -8,4 +8,8 @@
 import SwiftUI
 
 struct ImagePickerView: UIViewCOntrollerRepresentable {
+    // UIImagePickerController (写真撮影画) が表示されているか
+    @Binding var isShowSheet: Bool
+    // 撮影した写真
+    @Binding var captureImage: UIImage?
 }
