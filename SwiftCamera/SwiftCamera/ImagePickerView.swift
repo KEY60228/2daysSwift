cc8d263073f8f21630c7e2cdab69cfcd232e6c5d
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-07-24 22:49:37 +0900

    2-3-2. ImagePickerView.swiftファイルの作成

---
+++ SwiftCamera/SwiftCamera/ImagePickerView.swift
@@ -0,0 +1,20 @@
+//
+//  ImagePickerView.swift
+//  SwiftCamera
+//
+//  Created by 山口健太 on 2021/07/24.
+//
+
+import SwiftUI
+
+struct ImagePickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ImagePickerView_Previews: PreviewProvider {
+    static var previews: some View {
+        ImagePickerView()
+    }
+}
