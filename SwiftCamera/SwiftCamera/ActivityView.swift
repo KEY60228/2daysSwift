d9ffef60edb9c6379489db19d4ea22a2d16531cc
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-07-25 15:57:14 +0900

    4-2-2. UIActivityViewControllerでシェア画面を表示する

--- SwiftCamera/SwiftCamera/ActivityView.swift
+++ SwiftCamera/SwiftCamera/ActivityView.swift
@@ -8,4 +8,19 @@
 import SwiftUI
 
 struct ActivityView: UIViewControllerRepresentable {
+    // UIActivityViewControllerでシェアする写真
+    let shareItems: [Any]
+    
+    // 表示するViewを生成するタイミングで実行
+    func makeUIViewController(context: Context) -> UIViewController {
+        // UIActivityViewControllerでシェアする機能
+        let controller = UIActivityViewController(activityItems: shareItems, applicationActivities: nil)
+        // UIActivityViewCtonrollerを返す
+        return controller
+    }
+    
+    // Viewが更新されるタイミングで実行
+    func updateUIViewController(_ uiViewController: UIViewController, context: UIViewControllerRepresentableContext<ActivityView>) {
+        
+    }
 }
