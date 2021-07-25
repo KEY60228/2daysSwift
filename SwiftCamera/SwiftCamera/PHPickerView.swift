cf43c9ff6c7b8b3ee43c34ec646307d6f35090d0
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-07-25 16:11:58 +0900

    5-1-4. UIViewControllerRepresentableプロトコルの利用

--- SwiftCamera/SwiftCamera/PHPickerView.swift
+++ SwiftCamera/SwiftCamera/PHPickerView.swift
@@ -8,14 +8,6 @@
 import SwiftUI
 import PhotosUI
 
-struct PHPickerView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct PHPickerView_Previews: PreviewProvider {
-    static var previews: some View {
-        PHPickerView()
-    }
+struct PHPickerView: UIViewControllerRepresentable {
+    
 }
