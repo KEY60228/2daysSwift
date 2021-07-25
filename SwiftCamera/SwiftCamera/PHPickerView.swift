dd998ae30bfd2f0008d1b9888bae423d2110352a
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-07-25 16:10:33 +0900

    5-1-2. PHPickerViewControllerファイルの作成

---
+++ SwiftCamera/SwiftCamera/PHPickerView.swift
@@ -0,0 +1,20 @@
+//
+//  PHPickerView.swift
+//  SwiftCamera
+//
+//  Created by 山口健太 on 2021/07/25.
+//
+
+import SwiftUI
+
+struct PHPickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PHPickerView_Previews: PreviewProvider {
+    static var previews: some View {
+        PHPickerView()
+    }
+}
