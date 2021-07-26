7647743360dbd9a1c6ec3bbd49d74b21dd7ad157
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-07-26 21:27:10 +0900

    2-1-3. 連携するための状態変数の宣言

--- SwiftCamera/SwiftCamera/EffectView.swift
+++ SwiftCamera/SwiftCamera/EffectView.swift
@@ -8,6 +8,15 @@
 import SwiftUI
 
 struct EffectView: View {
+    // エフェクト編集画面の表示有無
+    @Binding var isShowSheet: Bool
+    // 撮影した写真
+    let captureImage: UIImage
+    // 表示する写真
+    @State var showImage: UIImage?
+    // シェア画面の表示有無
+    @State var isShowActivity = false
+    
     var body: some View {
     }
 }
