1874b321c5fd18f3f5cae0abf5f8ba9a6afdddf5
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-07-26 21:25:07 +0900

    2-1-2. EffectView.swiftファイルを作成

---
+++ SwiftCamera/SwiftCamera/EffectView.swift
@@ -0,0 +1,19 @@
+//
+//  EffectView.swift
+//  SwiftCamera
+//
+//  Created by 山口健太 on 2021/07/26.
+//
+
+import SwiftUI
+
+struct EffectView: View {
+    var body: some View {
+    }
+}
+
+struct EffectView_Previews: PreviewProvider {
+    static var previews: some View {
+        EffectView()
+    }
+}
