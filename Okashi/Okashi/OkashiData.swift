947492349c1109493cfc20c45c21d81468574db8
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-08-06 23:00:57 +0900

    4-2-2. リクエストを生成して、JSONを取得

--- Okashi/Okashi/OkashiData.swift
+++ Okashi/Okashi/OkashiData.swift
@@ -31,6 +31,28 @@ class OkashiData: ObservableObject {
             return
         }
         
+        // リクエストに必要な情報を生成
+        let req = URLRequest(url: req_url)
+        // データ転送を管理するためのセッションを生成
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        // リクエストをタスクとして登録
+        let task = session.dataTask(with: req, completionHandler: {(data, response, error) in
+            // セッションを終了
+            session.finishTasksAndInvalidate()
+            // do try catch エラーハンドリング
+            do {
+                // JSONDecorderのインスタンス取得
+                let decoder = JSONDecoder()
+                // 受け取ったJSONデータをパースして格納
+                let json = try decoder.decode(ResultJson.self, from: data!)
+                print(json)
+            } catch {
+                print("エラーが出ました")
+            }
+        })
+        // ダウンロード開始
+        task.resume()
+        
         print(req_url)
     }
 }
