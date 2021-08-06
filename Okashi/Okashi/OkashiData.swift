b0454262835bb480b8efd82ca5e42259f99824ef
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-08-06 22:25:17 +0900

    4-1-1. リクエストURLを生成して、JSONを取得しよう

--- Okashi/Okashi/OkashiData.swift
+++ Okashi/Okashi/OkashiData.swift
@@ -10,5 +10,17 @@ import Foundation
 class OkashiData: ObservableObject {
     func searchOkashi(keyword: String) {
         print(keyword)
+        
+        // お菓子のキーワードをURLエンコードする
+        guard let keyword_encode = keyword.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
+            return
+        }
+        
+        // リクエストURLの組み立て
+        guard let req_url = URL(string: "https://sysbird.jp/toriko/api/?apikey=guest&format=json&keyword=\(keyword_encode)&max=10&order=r") else {
+            return
+        }
+        
+        print(req_url)
     }
 }
