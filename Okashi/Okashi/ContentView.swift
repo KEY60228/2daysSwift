07185937efcddcd7b4400b27b15a77748ccf29e6
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-08-06 22:17:12 +0900

    3-2-3. TextFieldを配置しよう

--- Okashi/Okashi/ContentView.swift
+++ Okashi/Okashi/ContentView.swift
@@ -8,9 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    // OkashiDataを参照する状態変数
+    @ObservedObject var okashiDataList = OkashiData()
+    // 入力された文字列を保持する状態変数
+    @State var inputText = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            TextField("キーワードを入力してください", text: $inputText, onCommit: {
+                okashiDataList.searchOkashi(keyword: inputText)
+            })
+        }
     }
 }
 
