24ffc660ce616148e29902d6cb166c43c203636e
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-08-07 21:54:59 +0900

    6-1-1. Safari表示用の構造体を新規作成

---
+++ Okashi/Okashi/SafariView.swift
@@ -0,0 +1,20 @@
+//
+//  SafariView.swift
+//  Okashi
+//
+//  Created by 山口健太 on 2021/08/07.
+//
+
+import SwiftUI
+
+struct SafariView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SafariView_Previews: PreviewProvider {
+    static var previews: some View {
+        SafariView()
+    }
+}
